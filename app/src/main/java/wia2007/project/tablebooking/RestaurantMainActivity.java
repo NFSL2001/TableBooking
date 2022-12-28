e2ca8aed4fe7220547f20bec1e411cd9b471f1f3
NFSL2001 <s2116593@siswa.um.edu.my>
2022-12-28 19:50:16 +0800

    Finalize Main Menu

---
+++ app/src/main/java/wia2007/project/tablebooking/RestaurantMainActivity.java
@@ -0,0 +1,14 @@
+package wia2007.project.tablebooking;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RestaurantMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_restaurant_main);
+    }
+}
