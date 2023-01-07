a14bdfdd7906089b9cb10215a616186f30e5a035
jtchan17 <105500873+jtchan17@users.noreply.github.com>
2023-01-07 14:21:59 +0800

    Update TableAvailabilityList.java

--- app/src/main/java/wia2007/project/tablebooking/TableAvailabilityList.java
+++ app/src/main/java/wia2007/project/tablebooking/TableAvailabilityList.java
@@ -12,6 +12,8 @@ import wia2007.project.tablebooking.database.TableBookingDatabase;
 public class TableAvailabilityList extends AppCompatActivity {
 
     TextView date, time, numOfTableBooked, numOfTableAvailable, totalTable;
+    List<Table> tableList2, tableList4, tableList6, tableList8;
+    TableAdapter tableAdapter1, tableAdapter2, tableAdapter3, tableAdapter4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,18 +28,56 @@ public class TableAvailabilityList extends AppCompatActivity {
 
         String DATE = getIntent().getStringExtra("keydate");
         String TIME = getIntent().getStringExtra("keytime");
+        String[]splitTime = TIME.split(":");
+        Time startTime = new Time(Integer.parseInt(splitTime[0]),Integer.parseInt(splitTime[1]),00);
+        Time endTime = new Time(Integer.parseInt(splitTime[0])+1,Integer.parseInt(splitTime[1]),00);
 
         TableBookingDatabase database = TableBookingDatabase.getDatabase(getApplicationContext());
         TableDAO tableDAO = database.tableDAO();
-        BookingDAO bookingDAO = database.bookingDAO();
-        //List<Table> tableList = tableDAO.getAvailableTable(TIME, TIME);
+        List<Table> total = tableDAO.getTableByRestaurant(0);
+        List<Table> availableTableList = tableDAO.getAvailableTable(0,startTime, endTime);
 
         date.setText(DATE);
         time.setText(TIME);
-        //numOfTableAvailable.setText(tableList.toString());
-        totalTable.setText("20");
+        numOfTableBooked.setText((total.size()-availableTableList.size()));
+        numOfTableAvailable.setText(availableTableList.size());
+        totalTable.setText(total.size());
 
+        numOfTableBooked.setText("10");
+        numOfTableAvailable.setText("20");
+        totalTable.setText("30");
 
+        for(int i=0;i<availableTableList.size();i++){
+            if(availableTableList.get(i).getSize() == 2){
+                tableList2.add(availableTableList.get(i));
+            }else if(availableTableList.get(i).getSize() == 4){
+                tableList4.add(availableTableList.get(i));
+            }else if(availableTableList.get(i).getSize() == 6){
+                tableList6.add(availableTableList.get(i));
+            }else if(availableTableList.get(i).getSize() == 8){
+                tableList8.add(availableTableList.get(i));
+            }else{
+                throw new RuntimeException("No Table");
+            }
+        }
 
-    }
+        RecyclerView recyclerView1 = findViewById(R.id.RCtwopeopleTable);
+        tableAdapter1 = new TableAdapter(this,tableList2);
+        recyclerView1.setAdapter(tableAdapter1);
+        recyclerView1.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
+
+        RecyclerView recyclerView2 = findViewById(R.id.RCfourpeopleTable);
+        tableAdapter2 = new TableAdapter(this,tableList4);
+        recyclerView2.setAdapter(tableAdapter2);
+        recyclerView2.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
+
+        RecyclerView recyclerView3 = findViewById(R.id.RCsixpeopleTable);
+        tableAdapter3 = new TableAdapter(this,tableList6);
+        recyclerView3.setAdapter(tableAdapter3);
+        recyclerView3.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
+
+        RecyclerView recyclerView4 = findViewById(R.id.RCeightpeopleTable);
+        tableAdapter4 = new TableAdapter(this,tableList8);
+        recyclerView4.setAdapter(tableAdapter4);
+        recyclerView4.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
 }
